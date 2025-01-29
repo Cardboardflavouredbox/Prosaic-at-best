762bc54120c8a6a23def773e53b6c1f7f440ff54
Cardboardflavouredbox <minjoon0901@naver.com>
2025-01-29 15:41:55 +0900

    file arrangement stuff

---
+++ build/_deps/sfml-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/BlendMode.cpp.o.d
@@ -0,0 +1,6 @@
+_deps/sfml-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/BlendMode.cpp.o: \
+  /Users/greenbox/projects/Prosaic-at-best/build/_deps/sfml-src/src/SFML/Graphics/BlendMode.cpp \
+  /Users/greenbox/projects/Prosaic-at-best/build/_deps/sfml-src/include/SFML/Graphics/BlendMode.hpp \
+  /Users/greenbox/projects/Prosaic-at-best/build/_deps/sfml-src/include/SFML/Graphics/Export.hpp \
+  /Users/greenbox/projects/Prosaic-at-best/build/_deps/sfml-src/include/SFML/Config.hpp \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX15.2.sdk/usr/include/TargetConditionals.h
